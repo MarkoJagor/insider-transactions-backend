365b5c8e93d6d4cf28f68a8248ec5f864308a0d6
Marko Jagor <majago@ttu.ee>
2022-05-02 11:48:56 +0300

    changes to cors config

--- src/main/java/ee/taltech/insidertransactionsbackend/security/SecurityConfiguration.java
+++ src/main/java/ee/taltech/insidertransactionsbackend/security/SecurityConfiguration.java
@@ -1,5 +1,7 @@
 package ee.taltech.insidertransactionsbackend.security;
 
+import java.util.Arrays;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,6 +14,9 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import ee.taltech.insidertransactionsbackend.security.jwt.AuthEntryPointJwt;
 import ee.taltech.insidertransactionsbackend.security.jwt.AuthTokenFilter;
@@ -77,5 +82,17 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
         http.
                 addFilterBefore(this.authJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
+        configuration.setAllowedMethods(Arrays.asList("GET","POST","PUT"));
+        configuration.setAllowCredentials(true);
+        configuration.setAllowedHeaders(Arrays.asList("Authorization", "Cache-Control", "Content-Type"));
+        org.springframework.web.cors.UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 }
